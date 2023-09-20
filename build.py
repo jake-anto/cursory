4922e8c26510f78f7aa3cdaf1227795e77f837d2
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2023-09-20 14:08:45 +0000

    Improve logging

--- build.py
+++ build.py
@@ -1,36 +1,57 @@
+import logging
 import os
 import shutil
+from time import time
 
 import requests
 
 import main
 from languages import LANGS
 
+# Begin timer
+build_start = time()
+
+
 # Self-host simple.css
-print("Downloading simple.css")
+start = time()
+
 response = requests.get("https://cdn.simplecss.org/simple.min.css/")
 with open('assets/simple.css', "w", encoding="utf-8") as file:
     file.write(response.text)
 
-# Clear site directory
+print(f"Downloaded simple.css in {round(time() - start, 3)}s")
+
+
+# Clear site/ directory
+start = time()
+
 if os.path.isdir('site'):
-    print("Clearing site directory")
     try:
         shutil.rmtree('site')
     except OSError as e:
         print(f"Error {e.filename} - {e.filename}.{e.strerror}")
 
-# Copy assets
+print(f"Cleared site/ directory in {round(time() - start, 3)}s")
+
+# Copy static assets
+start = time()
+
 shutil.copytree('assets/', 'site/')
-print("Copied static files")
+
+print(f"Copied static files in {round(time() - start, 3)}s")
 
 for language in LANGS:
     try:
+        start = time()
+
         # Create a directory for language
         os.makedirs(os.path.dirname(f"site/{language}/"), exist_ok=True)
+
         # Build HTML file
         main.build(lang=language)
-    except Exception as e:
-        print(f"Skipped {language} because Exception: {e}")
 
-print("Build complete")
+        print(f"Built {language}.html in {round(time() - start, 3)}s")
+    except Exception as e:
+        logging.warning("Skipped %s.html because Exception: %s", language, e)
+
+print(f"Build completed in {round(time() - build_start, 3)}")
